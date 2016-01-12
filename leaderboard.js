f817f25ac2b8d84cbbd0af24c1c5bd196f006dcd
Jacob <jacob.varner14@gmail.com>
2016-01-12 14:46:37 -0600

    Learned about helpers, events, and sessions so that I could display the players and their score as well as highlight a specific player when they are clicked.

--- leaderboard.js
+++ leaderboard.js
@@ -1,2 +1,36 @@
 PlayersList = new Mongo.Collection('players');
-PlayersList.insert({name: "Jacob", score: 0});
+
+if(Meteor.isClient){
+  // This code only runs on the client.
+  Template.leaderboard.helpers({
+    // Helper functions go here.
+    'player': function(){
+      return PlayersList.find();
+    },
+    'playerCount': function(){
+      return "Number of players: " + PlayersList.find().count();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected";
+      }
+    }
+  });
+
+  Template.leaderboard.events({
+    // Event functions go here.
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+    'dblclick .player': function(){
+      console.log("You double clicked a .player element.");
+    }
+  });
+}
+
+if(Meteor.isServer){
+  // This code only runs on the server.
+}
